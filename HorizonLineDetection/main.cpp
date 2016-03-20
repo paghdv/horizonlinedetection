8c0bca97298a0ab312d5e9528279083f6fb30c59
Alejandro Galindo <pato.galindo@gmail.com>
2016-03-20 14:33:58 +0100

    Fixed mask input for running mode

--- HorizonLineDetection/main.cpp
+++ HorizonLineDetection/main.cpp
@@ -12,18 +12,22 @@ int main(int argc, char *argv[])
     const bool is_run=atoi(argv[1])==1;
     const std::string file_path_in=argv[2];
     const std::string file_path_out=argv[3];
-    std::string file_path_mask="";
-    if (argc>4)
-        file_path_mask=argv[4];
+
     if (is_run)
     {
+        std::string training_file="../Training/saved_model.svm";
+        std::string file_path_mask="";
+        if (argc>5)
+            file_path_mask=argv[5];
+        if (argc>6)
+            training_file=argv[6];
         //Test
         cv::Mat mask;
         if (!file_path_mask.empty())
             mask=cv::imread(file_path_mask);
         if (!mask.empty())
             std::cout<<"Using mask"<<std::endl;
-        hld.init_detector("../Training/saved_model.svm");
+        hld.init_detector(training_file);
         hld.detect_video(file_path_in,file_path_out,mask);
     }
     else
